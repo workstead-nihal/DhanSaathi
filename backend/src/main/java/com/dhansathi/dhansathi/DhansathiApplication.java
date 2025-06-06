ed9cf4248af0f11b66c8a86281baa9e346c80924
haru-kunyo <dainxin@gmail.com>
2025-06-06 21:43:50 +0530

    Added Spring Boot backend

---
+++ backend/src/main/java/com/dhansathi/dhansathi/DhansathiApplication.java
@@ -0,0 +1,13 @@
+package com.dhansathi.dhansathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhansathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhansathiApplication.class, args);
+	}
+
+}
